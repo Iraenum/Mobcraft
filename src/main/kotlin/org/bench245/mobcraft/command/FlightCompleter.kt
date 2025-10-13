9e51e182e66de9038502a2b4602dca2c041374f6
bench245 <bench245@duck.com>
2025-10-13 12:16:40 -0400

    Cascade 'if' replaced with 'when'

--- src/main/kotlin/org/bench245/mobcraft/command/FlightCompleter.kt
+++ src/main/kotlin/org/bench245/mobcraft/command/FlightCompleter.kt
@@ -10,14 +10,19 @@ class FlightCompleter(private val plugin: Mobcraft ) : TabCompleter {
     override fun onTabComplete(sender: CommandSender, command: Command, label: String, args: Array<out String>): List<String?>? {
         val suggestions = mutableListOf<String>()
         val onlinePlayers = Bukkit.getOnlinePlayers().map { it.name }
-        if (args.size == 1 && sender.isOp) {
-            suggestions.addAll(listOf("add", "remove", "setSpeed"))
-        } else if (args.size == 2 && args[0] == "add") {
-            suggestions.addAll(onlinePlayers.filter { player -> !plugin.flyingPlayers.contains(player)})
-        } else if (args.size == 2 && args[0] == "remove") {
-            suggestions.addAll(plugin.flyingPlayers)
-        } else if (args.size == 2) {
-            suggestions.addAll(onlinePlayers)
+        when (args.size) {
+            1 if sender.isOp -> {
+                suggestions.addAll(listOf("add", "remove", "setSpeed"))
+            }
+            2 if args[0] == "add" -> {
+                suggestions.addAll(onlinePlayers.filter { player -> !plugin.flyingPlayers.contains(player)})
+            }
+            2 if args[0] == "remove" -> {
+                suggestions.addAll(plugin.flyingPlayers)
+            }
+            2 -> {
+                suggestions.addAll(onlinePlayers)
+            }
         }
         return suggestions
     }
