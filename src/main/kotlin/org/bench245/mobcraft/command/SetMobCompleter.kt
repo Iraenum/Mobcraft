74643ae6cb9be926406e36ac27b013466c05520f
Iraenum <iraenum1@gmail.com>
2026-01-02 16:47:28 -0500

    Clean up SetMobCompleter.kt

--- src/main/kotlin/org/bench245/mobcraft/command/SetMobCompleter.kt
+++ src/main/kotlin/org/bench245/mobcraft/command/SetMobCompleter.kt
@@ -1,21 +1,18 @@
 package org.bench245.mobcraft.command
 
-import org.bench245.mobcraft.Mobcraft
 import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabCompleter
 
-class SetMobCompleter(private val plugin: Mobcraft) : TabCompleter {
-    private val mobs = listOf("axolotl", "blaze","elder_guardian", "ender_dragon", "enderman","ghast","skeleton","TUFFGOLEM","skeleton")
+class SetMobCompleter() : TabCompleter {
+    private val mobs = listOf("axolotl", "blaze", "ender_dragon", "enderman","ghast","tuffgolem")
     override fun onTabComplete(sender: CommandSender, command: Command, label: String, args: Array<out String>): List<String?>? {
         val suggestions = mutableListOf<String>()
         if (args.isEmpty()) {
             suggestions.addAll(Bukkit.getOnlinePlayers().map { it.name })
-        } else if (args.size == 1) {
-            suggestions.addAll(Bukkit.getOnlinePlayers().map { it.name })
         } else if (args.size == 2) {
-            suggestions.addAll(mobs.filter { mob -> !plugin.takenMobs.contains(mob) })
+            suggestions.addAll(mobs)
         }
         return suggestions
     }
